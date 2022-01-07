91b4007e89270863fbb4f9acb5e735c9052fb28b
AQ5 110171 郭凡潁(VanKuo) <van@mirle.com.tw>
2022-01-07 15:39:06 +0800

    將WCSManager中StoreIn_A2ToA4_WriteCV()的DB物件抽離，並更改連結route

--- Mirle.DB.Object/Service/clsStoreIn.cs
+++ Mirle.DB.Object/Service/clsStoreIn.cs
@@ -23,5 +23,35 @@ namespace Mirle.DB.Object.Service
                 clsWriLog.Log.subWriteExLog(cmet.DeclaringType.FullName + "." + cmet.Name, errorLine.ToString() + ":" + ex.Message);
             }
         }
+
+        public static void StoreIn_A2ToA4_WriteCV()//A2toA4寫入buffer
+        {
+            try
+            {
+                string stn = "";
+                for (int bufferIndex = 6; bufferIndex <= 10; bufferIndex += 2)
+                {
+                    if (bufferIndex == 6)
+                    {
+                        stn = StnNo.A6;
+                    }
+                    else if (bufferIndex == 8)
+                    {
+                        stn = StnNo.A8;
+                    }
+                    else if (bufferIndex == 10)
+                    {
+                        stn = StnNo.A10;
+                    }
+                    clsDB_Proc.GetDB_Object().GetProcess().FunStockInA2ToA4WriPlc(stn, bufferIndex);
+                }
+            }
+            catch (Exception ex)
+            {
+                int errorLine = new System.Diagnostics.StackTrace(ex, true).GetFrame(0).GetFileLineNumber();
+                var cmet = System.Reflection.MethodBase.GetCurrentMethod();
+                clsWriLog.Log.subWriteExLog(cmet.DeclaringType.FullName + "." + cmet.Name, errorLine.ToString() + ":" + ex.Message);
+            }
+        }
     }
 }
