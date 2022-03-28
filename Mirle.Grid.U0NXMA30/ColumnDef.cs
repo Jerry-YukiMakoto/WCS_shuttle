72f5d42bbe425a07499f885f722a4e437418e7aa
AQ5 110171 郭凡潁(VanKuo) <van@mirle.com.tw>
2022-03-28 12:02:48 +0800

    主畫面命令顯示修改

--- Mirle.Grid.U0NXMA30/ColumnDef.cs
+++ Mirle.Grid.U0NXMA30/ColumnDef.cs
@@ -10,26 +10,28 @@ namespace Mirle.Grid.U0NXMA30
     {
         public class CMD_MST
         {
-            public static readonly ColumnInfo CmdSno = new ColumnInfo { Index = 0, Name = "任務號", Width = 68 };
-            public static readonly ColumnInfo CmdSts = new ColumnInfo { Index = 1, Name = "命令狀態", Width = 40 };
-            public static readonly ColumnInfo CmdMode = new ColumnInfo { Index = 2, Name = "模式", Width = 40 };
-            public static readonly ColumnInfo Trace = new ColumnInfo { Index = 3, Name = "Trace", Width = 40 };
-            public static readonly ColumnInfo EquNO = new ColumnInfo { Index = 4, Name = "設備編號", Width = 60 };
-            public static readonly ColumnInfo StnNo = new ColumnInfo { Index = 5, Name = "站口", Width = 60 };
-            public static readonly ColumnInfo whetherAllout = new ColumnInfo { Index = 6, Name = "整板出庫", Width = 40 };
-            public static readonly ColumnInfo lastPallet = new ColumnInfo { Index = 7, Name = "是否尾板", Width = 40 };
-            public static readonly ColumnInfo Loc = new ColumnInfo { Index = 8, Name = "儲位", Width = 68 };
-            public static readonly ColumnInfo NewLoc = new ColumnInfo { Index = 9, Name = "新儲位", Width = 68 };
-            public static readonly ColumnInfo Remark = new ColumnInfo { Index = 10, Name = "說明", Width = 200 };
-            public static readonly ColumnInfo CrtDate = new ColumnInfo { Index = 11, Name = "產生時間", Width = 200 };
-            public static readonly ColumnInfo ExpDate = new ColumnInfo { Index = 12, Name = "執行時間", Width = 200 };
-            public static readonly ColumnInfo EndDate = new ColumnInfo { Index = 13, Name = "結束時間", Width = 200 };
+            public static readonly ColumnInfo CmdSno = new ColumnInfo { Index = 0, Name = "任務號", Width = 75 };
+            public static readonly ColumnInfo palletNo = new ColumnInfo { Index = 1, Name = "棧板編號", Width = 120 };
+            public static readonly ColumnInfo CmdSts = new ColumnInfo { Index = 2, Name = "命令狀態", Width = 60 };
+            public static readonly ColumnInfo CmdMode = new ColumnInfo { Index = 3, Name = "模式", Width = 50 };
+            public static readonly ColumnInfo Trace = new ColumnInfo { Index = 4, Name = "Trace", Width = 50 };
+            public static readonly ColumnInfo EquNO = new ColumnInfo { Index = 5, Name = "設備編號", Width = 75 };
+            public static readonly ColumnInfo StnNo = new ColumnInfo { Index = 6, Name = "站口", Width = 60 };
+            public static readonly ColumnInfo whetherAllout = new ColumnInfo { Index = 7, Name = "整板出庫", Width = 50 };
+            public static readonly ColumnInfo lastPallet = new ColumnInfo { Index = 8, Name = "是否尾板", Width = 50 };
+            public static readonly ColumnInfo Loc = new ColumnInfo { Index = 9, Name = "儲位", Width = 68 };
+            public static readonly ColumnInfo NewLoc = new ColumnInfo { Index = 10, Name = "新儲位", Width = 68 };
+            public static readonly ColumnInfo Remark = new ColumnInfo { Index = 11, Name = "說明", Width = 200 };
+            public static readonly ColumnInfo CrtDate = new ColumnInfo { Index = 12, Name = "產生時間", Width = 200 };
+            public static readonly ColumnInfo ExpDate = new ColumnInfo { Index = 13, Name = "執行時間", Width = 200 };
+            public static readonly ColumnInfo EndDate = new ColumnInfo { Index = 14, Name = "結束時間", Width = 200 };
 
             public static void GridSetLocRange(ref DataGridView oGrid)
             {
-                oGrid.ColumnCount = 14;
+                oGrid.ColumnCount = 15;
                 oGrid.RowCount = 0;
                 clInitSys.SetGridColumnInit(CmdSno, ref oGrid);
+                clInitSys.SetGridColumnInit(palletNo, ref oGrid);
                 clInitSys.SetGridColumnInit(CmdSts, ref oGrid);
                 clInitSys.SetGridColumnInit(CmdMode, ref oGrid);
                 clInitSys.SetGridColumnInit(Trace, ref oGrid);
